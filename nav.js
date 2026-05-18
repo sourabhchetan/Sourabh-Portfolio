b426b5b6a0a5d24eebcc92e4e5ca4ddd434deab3
sourabhchetan <sourabhchetan1605hotmail.com>
2026-05-18 14:23:10 +0530

    Fixed mobile navbar

--- nav.js
+++ nav.js
@@ -59,3 +59,11 @@ window.addEventListener("scroll", () => {
     });
 
 });
+// ===== MOBILE NAVBAR =====
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("show-menu");
+});
